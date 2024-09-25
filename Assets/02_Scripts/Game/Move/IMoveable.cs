933b19900518777c185c2ce377e9ebe648bdd393
dkdkdsa <bos94330@gmail.com>
2024-09-26 00:00:08 +0900

    Add IMoveable

--- Assets/02_Scripts/Game/Move/IMoveable.cs
+++ Assets/02_Scripts/Game/Move/IMoveable.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class IMoveable : MonoBehaviour
+public interface IMoveable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void Move(in Vector3 vec, in float speed);
+
 }
