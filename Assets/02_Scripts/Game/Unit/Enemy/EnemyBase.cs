df35aae030d9cf44fbfa2a028342ed189dccf0f9
dkdkdsa <bos94330@gmail.com>
2024-09-27 01:08:13 +0900

    유닛 SO 추가

--- Assets/02_Scripts/Game/Unit/Enemy/EnemyBase.cs
+++ Assets/02_Scripts/Game/Unit/Enemy/EnemyBase.cs
@@ -3,9 +3,15 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(LocalInstaller))]
-public abstract class EnemyBase : UnitBase
+public abstract class EnemyBase : UnitBase, ILocalInject
 {
 
+    protected IController _moveController;
 
+    public void LocalInject(ComponentList list)
+    {
 
+        _moveController = list.Find<IController>();
+
+    }
 }
---
+++ Assets/02_Scripts/Game/Unit/UnitDataSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Unit/Data")]
+public class UnitDataSO : ScriptableObject
+{
+    [field: SerializeField] public string Name { get; private set; }
+    [field: SerializeField] public Sprite View { get; private set; }
+
+}
