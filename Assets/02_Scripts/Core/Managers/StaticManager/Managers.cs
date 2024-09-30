2f3d51a394b65db6f74d61246b45896103c0f17d
dkdkdsa <bos94330@gmail.com>
2024-09-30 15:13:34 +0900

    매니져 기본 구조 작성

--- Assets/02_Scripts/Core/Managers/StaticManager/Managers.cs
+++ Assets/02_Scripts/Core/Managers/StaticManager/Managers.cs
@@ -1,25 +1,52 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public static class Managers
 {
-    
+
     private static Dictionary<Type, IManager> _managerContainer = new();
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Bind()
     {
 
+        GameObject root = new GameObject("@!STATIC_MANAGERS!@");
 
+#if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.quitting += HandleQuit;
+
+#else
+    
+        Application.quitting += HandleQuit;
+
+#endif
+        var resourceManager = root.AddComponent<AddressableResourceManager>();
+        _managerContainer.Add(resourceManager.GetType(), resourceManager);
+        _managerContainer.Add(typeof(SoundManager), new SoundManager());
+
+        foreach (var manager in _managerContainer.Values)
+        {
+            manager.Init();
+        }
+
+    }
+
+    private static void HandleQuit()
+    {
+
+        foreach(var item in _managerContainer.Values)
+        {
+            item.Dispose();
+        }
 
     }
 
     public static T GetManager<T>() where T : IManager
     {
 
-        if(_managerContainer.TryGetValue(typeof(T), out var value))
+        if (_managerContainer.TryGetValue(typeof(T), out var value))
         {
             return value.Cast<T>();
         }
