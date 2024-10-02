ca81223427d8a8e835a6b66f45cca798878e5121
dkdkdsa <bos94330@gmail.com>
2024-10-02 11:22:54 +0900

    UnitFactory수정

--- Assets/02_Scripts/Core/Factory/Factorys/UnitFactory.cs
+++ Assets/02_Scripts/Core/Factory/Factorys/UnitFactory.cs
@@ -1,22 +1,34 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitFactory : MonoBehaviour, IFactory<IRouteBaseSpawnable>
+public class UnitFactory : MonoBehaviour, IFactory<ISpawnable>
 {
     private Dictionary<string, GameObject> _prefabContainer = new();
 
-    public IRouteBaseSpawnable CreateInstance(PrefabData data = default, object extraData = null)
+    private void Awake()
+    {
+
+        Factorys.AddFactory(typeof(IFactory<ISpawnable>), this);
+
+    }
+
+    public ISpawnable CreateInstance(PrefabData data = default, object extraData = null)
     {
         var ins = _prefabContainer[data.prefabKey];
 
         if (ins == null) return null;
 
         //객체 생성 부분 나중에 Pooling으로 바꿀것
-        var obj = Instantiate(ins).GetComponent<IRouteBaseSpawnable>();
+        var obj = Instantiate(ins).GetComponent<ISpawnable>();
         obj.Init();
 
         return obj;
+    }
+
+    private void OnDestroy()
+    {
+
+        Factorys.RemoveFactory(typeof(IFactory<ISpawnable>));
+
     }
 }
