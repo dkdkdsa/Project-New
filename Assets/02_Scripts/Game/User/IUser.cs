60cb855375f53299b7e57cccd2de87fcb4e05402
dkdkdsa <bos94330@gmail.com>
2024-10-10 19:51:39 +0900

    사용자 구조 수정

--- Assets/02_Scripts/Game/User/IUser.cs
+++ Assets/02_Scripts/Game/User/IUser.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 public interface IUser
 {
 
     public DeckData Deck { get; }
+    public List<string> Towers { get; }
     public IMoney Coin { get; }
 
 }
--- Assets/02_Scripts/Game/User/User.cs
+++ Assets/02_Scripts/Game/User/User.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
+
 public class User : IUser
 {
     public DeckData Deck { get; private set; } = new();
-
+    public List<string> Towers { get; private set; } = new();
     public IMoney Coin { get; private set; }
 
     public User(IMoney coin)
