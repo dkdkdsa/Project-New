a9275af968cac4b30de6d47cc10003f37ba212c1
dkdkdsa <bos94330@gmail.com>
2024-10-10 11:50:31 +0900

    업글 구조 수정중

--- Assets/02_Scripts/Game/Upgrade/Components/TowerUpgrader.cs
+++ Assets/02_Scripts/Game/Upgrade/Components/TowerUpgrader.cs
@@ -1,11 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class TowerUpgrader : StatUpgrader<int, float>
 {
-    protected override bool ApplyCost()
+
+    private IMoney _money;
+
+    private void Awake()
     {
+
+        _money = Singletons.GetSingleton<IMoney>();
+
+    }
+
+    protected override bool ApplyCost(int cost)
+    {
+
+        if (cost > _money.Money)
+            return false;
+
+        _money.RemoveMoney(cost);
 
         return true;
 
